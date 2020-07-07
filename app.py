6af758ae9eb277f52ef4dea9b8484b39cfa1a25a
sadipgiri <sadipgiri@bennington.edu>
2020-07-07 11:24:50 -0400

    templates on sanic

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, html, text
+import os
 
 app = Sanic("Sadip Simple Sanic App")
 
+# return json response
 @app.route("/")
 async def test(request):
     return json({"hello": "world"})
+
+# return html
+@app.route("/html")
+async def test(request):
+    template = open(os.getcwd() + "/templates/index.html")
+    return html(template.read())
+
+# return text
+@app.route("/text")
+async def test(request):
+    return text("This is Text Response.")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
